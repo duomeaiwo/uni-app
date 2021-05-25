1106a012312c31d530a1e25b4a0b14c8b4cb38f9
Dex <1073773986@qq.com>
2021-05-25 17:37:47 +0800

    init proj

---
+++ pages/money/money.vue
@@ -0,0 +1,57 @@
+<template>
+	<view>
+		<view>rpx</view>
+		<view class="box">
+			{{msg}}
+			<view class="iconfont icon-jiagezixun">
+				<button type="default" @click="pulldown">refresh</button>
+			</view>
+		</view>
+		<view v-for="item in list" class="item">
+			{{item}}
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				msg: 'xxx',
+				list: [
+					'apple', 'orange', 'pineapple', 'watermelon', 'banana', 'lemon'
+				]
+			};
+		},
+		onPullDownRefresh() {
+			this.msg = 'aaa'
+			uni.stopPullDownRefresh()
+		},
+		onReachBottom() {
+			console.log('xx')
+			this.list = [...this.list, 
+				'apple', 'orange', 'pineapple', 'watermelon', 'banana', 'lemon'
+			]
+		},
+		methods: {
+			pulldown() {
+				uni.startPullDownRefresh()
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.box {
+		width: 375rpx;
+		height: 375rpx;
+		color: $uni-color-error;
+		font-size: 75rpx;
+		background-color: #4CD964;
+	}
+
+	.item {
+		height: 100px;
+		line-height: 100px;
+	}
+</style>
---
+++ pages/trip/trip.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="trip">
+		<view class="box" hover-class="box-active" hover-stop-propagation :hover-start-time="100" :hover-stay-time="1000">
+			<text selectable>this is detail page</text>
+		</view>
+		<view >
+			<text selectable="true" space="emsp">可被  选中</text>
+		</view>
+		<button type="default" size="mini" plain>click</button>
+		<button type="primary" size="default" >click</button>
+		<button type="warn" size="default"  loading>click</button>
+		<button type="warn" size="default"  disabled>click</button>
+		<image src="../../static/mixkit-i-love-you-hand-gesture-419-original.png" ></image>
+		<image src="../../static/mixkit-i-love-you-hand-gesture-419-original.png" mode="aspectFit"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style>
+	.box {
+		width: 100px;
+		height: 100px;
+		background-color: green;
+	}
+	.box-active {
+		width: 100%;
+		height: 100px;
+		background-color: skyblue;
+	}
+</style>
